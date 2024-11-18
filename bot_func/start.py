7b0a5684d406a175c02cbf687bfb20aff7902b96
Asrorbek Aliqulov <whoami.ali008@gmail.com>
2024-11-18 22:08:02 +0500

    Delete bot_func directory

--- bot_func/start.py
+++
@@ -1,9 +0,0 @@
-from telegram import Update
-from telegram.ext import ContextTypes
-
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(
-        f"Salom {update.message.from_user.first_name}!",
-        parse_mode='HTML'
-    )
